9b9de055072bb02f41a6e826286c64f4e303c8a1
MistyTTM <emmey.leo@gmail.com>
2022-03-15 21:14:26 +1000

    Move tests to new file

--- bot.py
+++ bot.py
@@ -8,44 +8,17 @@ import re
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-# load the .env file
+# bot is imported
+from discord.ext import commands
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+# command prefix
+bot = commands.Bot(command_prefix='QUT!')
 
-# show the bot has conntected to discord
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-#@client.event
-#async def on_member_join(member):
-#    await member.create_dm()
-#    await member.dm_channel.send(
-#        f'Hi {member.name}, welcome to the QUT Bachelor'
-#    )
-
-# When student number is sent, return message
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if re.search("^n[0-9]{8}",message.content):
-        response = "Verification code sent"
-        await message.channel.send(response)
-    elif message.content == 'raise-exception':
-        raise discord.DiscordException
-
-# Output errors to err.log
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('logs/err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
---
+++ botTestClient.py
@@ -0,0 +1,51 @@
+# Libraries
+import os
+import asyncio
+import discord
+from dotenv import load_dotenv
+import random
+import re
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+# load the .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+# show the bot has conntected to discord
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+#@client.event
+#async def on_member_join(member):
+#    await member.create_dm()
+#    await member.dm_channel.send(
+#        f'Hi {member.name}, welcome to the QUT Bachelor'
+#    )
+
+# When student number is sent, return message
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if re.search("^n[0-9]{8}",message.content):
+        response = "Verification code sent"
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+# Output errors to err.log
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('logs/err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
+
+client.run(TOKEN)
