395b9c23268ca7b7cbe78fcb822ed6305bcab6a0
MistyTTM <emmey.leo@gmail.com>
2022-03-15 19:24:37 +1000

    Testing and learning

--- bot.py
+++ bot.py
@@ -1,15 +1,23 @@
 # bot.py
 import os
+import asyncio
 import discord
 from dotenv import load_dotenv
 
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.client()
+client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+        )
 
 client.run(TOKEN)
