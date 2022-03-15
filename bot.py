db4fa291a5b4058105e8067c0b937e6a33728568
MistyTTM <emmey.leo@gmail.com>
2022-03-15 21:02:11 +1000

    Communication

--- bot.py
+++ bot.py
@@ -3,37 +3,34 @@ import os
 import asyncio
 import discord
 from dotenv import load_dotenv
+import random
+import re
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has conected to discord!')
+    print(f'{client.user.name} has connected to Discord!')
+
+#@client.event
+#async def on_member_join(member):
+#    await member.create_dm()
+#    await member.dm_channel.send(
+#        f'Hi {member.name}, welcome to the QUT Bachelor'
+#    )
 
 @client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, Welcome to my Discord server!'
-    )
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if re.search("^n[0-9]{8}",message.content):
+        response = "Verification code sent"
+        await message.channel.send(response)
 
-# class CustomClient(discord.Client):
-#     async def on_ready(self):
-#         print(f'{self.user} has conected to discord!')
-
-# @client.event
-# async def on_ready():
-#     guild = discord.utils.get(client.guilds, name=GUILD)
-#     print(
-#         f'{client.user} is connected to the following guild:\n'
-#         f'{guild.name}(id: {guild.id})'
-#         )
-
-# client = CustomClient()
 client.run(TOKEN)
