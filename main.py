a47acc7dae2a7b65f7ca7621cbaabdb98a2d7802
MistyTTM <emmey.leo@gmail.com>
2022-03-18 17:40:20 +1000

    commit

--- main.py
+++ main.py
@@ -13,22 +13,28 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 
-from replit import db
 from discord.utils import get
+from discord.utils import find
+from discord.ext.commands import Bot
+
 
 SKIP_BOTS = False
+
+intents = discord.Intents.all()
 
 nav = DefaultMenu(page_left="◀️", page_right="▶️", remove="❌")
 
 ending_note = "{ctx.bot.user.name} is not affiliated with QUT"
 
-bot = commands.Bot(command_prefix="qut!", description="A bot to help with QUT servers")
+bot = commands.Bot(command_prefix="qut!", description="A bot to help with QUT servers", intents=intents)
 client = discord.Client
+Client = Bot('qut!')
 
 bot.help_command = PrettyHelp(menu=nav, color=discord.Colour.blue(), ending_note=ending_note)
 
-token = os.environ.get("DISCORD_TOKEN")
+token = os.getenv("DISCORD_TOKEN")
 
+codes = []
 
 class Moderation(commands.Cog):
   """All Moderation Commands"""
@@ -41,26 +47,97 @@ class Moderation(commands.Cog):
     num = random.randint(1,100)
     await ctx.send(f"{ctx.message.author.mention} your number is {num}")
 
-j = 0
+  #@commands.command(
+  #  name='clear',
+  #  brief='Clears messages',
+  #  help='this command will clear msgs'
+  #)
+  #async def clear(ctx, number):
+  #  mgs = [] #Empty list to put all the messages in the log
+  #  number = int(number) #Converting the amount of messages to delete to an integer
+  #  async for x in Client.logs_from(ctx.message.channel, limit = number):
+  #      mgs.append(x)
+  #  await Client.delete_messages(mgs)
 
-@bot.event
+class Info(commands.Cog):
+  """Information commands"""
+  @commands.command(
+    name="info",
+    brief="Info about the bot",
+    help="Command to provide information about the bot"
+  )
+  async def _info(self, ctx):
+        embed=discord.Embed(title="QUTBot v1.1.0", url="https://realdrewdata.medium.com/", description=f"This bot was designed and programmed by *Emmey Leo* for the QUT IN01 Discord. It provides a system to verify that new members are qut students. This project is completely open source and any and all people are allowed to contribute to the github:\n\n**https://github.com/Mistyttm/DiscordQUTVerificationBot**", color=discord.Color.dark_blue())
+        await ctx.send(embed=embed)
+  
+  @commands.command(
+    name="verify",
+    brief="Instructions on how to verify",
+    help="Command to provide information about how to verify your account"
+  )
+  async def _info(self, ctx):
+        embed=discord.Embed(title="Verification Instructions", url="https://realdrewdata.medium.com/", description=f"1. Go to #verification\n2. Send your student number e.g. n12345678\n3. Check your QUT email for the verification code\n4. Send the verification code in #verification", color=discord.Color.dark_blue())
+        await ctx.send(embed=embed)
+
+
+#class Verification(commands.Cog):
+#  """All Verification Commands"""
+#  @commands.command(
+#    name="clearcodes",
+#    brief="Clears verification codes",
+#    help="This command removes all stored verification codes, users who have not used their code will need another one"
+#  )
+#  async def _removeCodes(self, ctx):
+#    global codes
+#    codes.clear()
+#    print(codes)
+#    await ctx.send("All verification codes have been deleted")
+
+#  @commands.command(
+#    name="addcode",
+#    brief="Adds a custom code",
+#    help="This command adds a custom 4 number code for moderators to give out"
+#  )
+#  async def _addCodes(self, ctx, arg,):
+#    global codes
+#    role = discord.utils.find(lambda r: r.name == 'Moderator', ctx.message.guild.roles)
+#    if len(arg) == 4 and role in self.roles:
+#      codes.append(arg)
+#      print(codes)
+#      await ctx.send(f"Your custom code is: {arg}")
+#    elif role not in self.roles:
+#      await ctx.send(f"You cannot use this command")
+#    else:
+#        await ctx.send(f"That is an incorrectly formatted code")
+
+numb = 0
+
+
+def increment():
+  global numb
+  numb = numb + 1
+
+
+@bot.listen()
 async def on_message(message):
     if message.author == client.user:
         return
     
     verify = []
     
-    j = 0
-    
     if re.search("^n[0-9]{8}",message.content):
         for i in range(4):
             verify.append(str(random.randint(0, 9)))
         
         verify_code = ''.join(verify)
-        
-        
+
+        global numb
+        j = numb
+
+      
         sender = 'discordbotforin01@gmail.com'
         receiver = message.content + '@qut.edu.au'
+        #receiver = 'discordbotforin01@gmail.com'
         body_send = "your verification code is: " + verify_code
 
         msg = MIMEText(body_send, 'html')
@@ -72,12 +149,11 @@ async def on_message(message):
         s.sendmail(sender, receiver, msg.as_string())
         s.quit()
         
-        db[f"code{j}"] = f"{verify_code}"
-        db[f"user{j}"] = f"{message.content}"
-        db[f"entry{j}"] = f"{j}"
-        j = j + 1
+        codes.append(f"{verify_code}")
+        print(codes)
+        increment()
         
-        response = "Verification code sent"
+        response = "Verification code sent to your QUT student email"
         await message.channel.send(response)
         print(f'{verify} is the code')
     elif message.content == 'raise-exception':
@@ -85,35 +161,48 @@ async def on_message(message):
 
     if re.search("[0-9]{4}",message.content):
       print("recieved")
-      if message.content == db[f"code{j}"]:
+      if message.content in codes:
         member = message.author
+        removal = codes.index(message.content)
+        print(removal)
+        codes.pop(removal)
+        old_role = discord.utils.get(member.guild.roles, name="Visitor")
+        temp_role = discord.utils.get(member.guild.roles, name="Visitor Temp")
+        await member.remove_roles(old_role)
+        await member.remove_roles(temp_role)
+        role = discord.utils.get(member.guild.roles, name="Verified")
+        await member.add_roles(role)
         await member.create_dm()
         await member.dm_channel.send(
           f'Hi there! Thank you for verifying your account, welcome to the server :)'
         )
-        del db[f"code{j}"]
-        del db[f"user{j}"]
-        del db[f"entry{j}"]
-        old_role = discord.utils.get(member.guild.roles, name="Visitor")
-        await member.remove_roles(old_role)
-        role = discord.utils.get(member.guild.roles, name="Verified")
-        await member.add_roles(role)
+        
 
 @bot.event
 async def on_ready():
   print("I'm in")
 
-@client.event
-async def on_error(event, *args, **kwargs):
-    with open('logs/err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+@bot.event
+async def on_guild_join(guild):
+    general = find(lambda x: x.name == 'general-general',  guild.text_channels)
+    if general and general.permissions_for(guild.me).send_messages:
+        await general.send("Hi there! I'm QUTBot, I don't have many features right now, but I hope you'll help me grow :D")
+
+@bot.event 
+async def on_member_join(member):
+  role = get(member.guild.roles, name="Visitor")
+  await member.add_roles(role)
+  await member.create_dm()
+  await member.dm_channel.send(
+f'{member.mention} Please send your QUT student number in #visitor (E.g: "n12345678"), then send the verification code that will be emailed to you.\n\n**Do Not Respond To This Message**'
+        )
   
 def run():
   bot.add_cog(Moderation(bot))
+  #bot.add_cog(Verification(bot))
+  bot.add_cog(Info(bot))
   bot.run(token)
+  client.run(token)
 
 if __name__ == "__main__":
   run()
