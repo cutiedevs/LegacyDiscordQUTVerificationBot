993c9fbdc6bf90d97ccb5541340b0b707ee346b7
Misty <51770769+Mistyttm@users.noreply.github.com>
2022-03-18 12:25:39 +1000

    v1.1.0
    - changed version number
    - added verification help command
    - added support for the "Visitor Temp" role

--- bot/main.py
+++ bot/main.py
@@ -67,8 +67,18 @@ class Info(commands.Cog):
     help="Command to provide information about the bot"
   )
   async def _info(self, ctx):
-        embed=discord.Embed(title="QUTBot v1.0.1", url="https://realdrewdata.medium.com/", description=f"This bot was designed and programmed by *Emmey Leo* for the QUT IN01 Discord. It provides a system to verify that new members are qut students. This project is completely open source and any and all people are allowed to contribute to the github:\n\n**https://github.com/Mistyttm/DiscordQUTVerificationBot**", color=discord.Color.dark_blue())
+        embed=discord.Embed(title="QUTBot v1.1.0", url="https://realdrewdata.medium.com/", description=f"This bot was designed and programmed by *Emmey Leo* for the QUT IN01 Discord. It provides a system to verify that new members are qut students. This project is completely open source and any and all people are allowed to contribute to the github:\n\n**https://github.com/Mistyttm/DiscordQUTVerificationBot**", color=discord.Color.dark_blue())
         await ctx.send(embed=embed)
+  
+  @commands.command(
+    name="verify",
+    brief="Instructions on how to verify",
+    help="Command to provide information about how to verify your account"
+  )
+  async def _info(self, ctx):
+        embed=discord.Embed(title="Verification Instructions", url="https://realdrewdata.medium.com/", description=f"1. Go to #verification\n2. Send your student number e.g. n12345678\n3. Check your QUT email for the verification code\n4. Send the verification code in #verification", color=discord.Color.dark_blue())
+        await ctx.send(embed=embed)
+
 
 #class Verification(commands.Cog):
 #  """All Verification Commands"""
@@ -157,7 +167,9 @@ async def on_message(message):
         print(removal)
         codes.pop(removal)
         old_role = discord.utils.get(member.guild.roles, name="Visitor")
+        temp_role = discord.utils.get(member.guild.roles, name="Visitor Temp")
         await member.remove_roles(old_role)
+        await member.remove_roles(temp_role)
         role = discord.utils.get(member.guild.roles, name="Verified")
         await member.add_roles(role)
         await member.create_dm()
